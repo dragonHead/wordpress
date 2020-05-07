9de7beb3c671d0bf5c07de7ca5ebf9c35ca6c8bf
jack-8 <himawari888ty@gmail.com>
2020-05-08 07:56:11 +0900

    update setting.sh

--- docker/wordpress/shell/initialsetting.sh
+++ docker/wordpress/shell/initialsetting.sh
@@ -37,7 +37,7 @@ wp rewrite structure '/archives/%post_id%'
 # https://ja.wordpress.org/plugins/
 wp plugin install wp-multibyte-patch --activate
 wp plugin install backwpup --activate
-wp plugin install classic-editor --activate
+wp plugin install all-in-one-wp-security-and-firewall --activate
 wp plugin install password-protected
 
 # コアアップデート
